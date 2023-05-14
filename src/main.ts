9ee5d83092c0394de7df8940e421682252b77426
samuel14luis <luismendoza.samuel@gmail.com>
2023-05-13 22:16:03 -0500

    feat: add global prefix on /api route

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
 
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${ process.env.PORT }`);
+  logger.log(`App global prefix is ${ process.env.HOST_API }`);
 }
 
 bootstrap();
